d05b7919f47a952136ba412489269ed4a0a9310f
James <jpark196@fordham.edu>
2019-12-10 22:45:45 -0500

    test demo 1
    everything seems to work...

---
+++ Assets/scripts/Getmenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Getmenu : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject otherInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menu.activeSelf == false)
+            {
+                otherInput.SetActive(false);
+                menu.SetActive(true);
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Confined;
+            }
+            else
+            {
+                otherInput.SetActive(true);
+                menu.SetActive(false);
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+}
---
+++ Assets/scripts/MenuSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSelect : MonoBehaviour
+{
+    public void Load0()
+    {
+        SceneManager.LoadSceneAsync("Scenes/testLvl0");
+    }
+
+    public void Load1()
+    {
+        SceneManager.LoadSceneAsync("Scenes/testLvl1");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
